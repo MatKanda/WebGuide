def745675fc19e675e47232284f5ad2b86af545d
MatKanda <75527738+MatKanda@users.noreply.github.com>
2021-05-20 18:35:26 +0200

    deleted file

--- index.ts
+++
@@ -1,9 +0,0 @@
-import {webGuide} from "./startGuide";
-
-export * from './startGuide';
-export * from './init';
-export * from './popupDiv';
-export * from './animations';
-
-// @ts-ignore
-window.webGuide=webGuide;
