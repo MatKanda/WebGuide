1edef4c542450713501bfa3b2ddea6fa96edd64b
MatKanda <75527738+MatKanda@users.noreply.github.com>
2021-06-22 21:58:53 +0200

    Update arrangeOrder.ts

--- src/init/arrangeOrder.ts
+++ src/init/arrangeOrder.ts
@@ -2,7 +2,7 @@ export function arrangeOrder(array:HTMLElement[]){
     for (var i=1;i<=numberOfElements;i++)
         for(var j=0;j<array.length;j++)
             if(array[j].hasAttribute("data-guide-step")) {
-                if (array[j].getAttribute("data-guide-step") === i.toString()) //problem s porovnanim bolo treba tostring
+                if (array[j].getAttribute("data-guide-step") === i.toString())
                     elements.push(array[j]);
             }
             else{
